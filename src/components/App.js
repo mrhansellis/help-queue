a3a52782e3e0f7d37b00286a211d79de779ba591
Hans Ellis <bullhorn@gmail.com>
2023-03-08 19:49:41 -0800

    add footer with logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import TicketList from "./TicketList";
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
     <React.Fragment>
       <Header />
       <TicketList />
+      <Footer />
     </React.Fragment>
   );
 }
