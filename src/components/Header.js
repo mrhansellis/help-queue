1eb11e0ac7f91fde24db0801ec8ca477d593023d
Hans Ellis <bullhorn@gmail.com>
2023-03-13 20:34:02 -0700

    add ticket functions and update state control

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ticketMan from "./../img/ticketman.png"
 
 function Header(){
   return (
+  <React.Fragment>
     <h1>Help Queue</h1>
+    <img src={ticketMan} alt="An image of a stressed out man managing a mess of paper tickets" />
+  </React.Fragment>
   );
 }
 
---
+++ src/components/QuestionOne.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function QuestionOne(props){
+  return (
+    <React.Fragment>
+      <h3>"Have you gone through all the steps in the Learn How To Program lesson?</h3>
+    </React.Fragment>
+  )
+}
+
+export default QuestionOne;
